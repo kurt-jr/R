098fea7fb521631858400750677b8d8e4bd824dc
kurt-jr <nps3cs@virginia.edu>
2024-06-14 15:50:50 -0400

    ggplot

--- facebookHistogram.R
+++ facebookHistogram.R
@@ -3,5 +3,6 @@ library(readxl)
 MSDS_Orientation_Computer_Survey <- read_excel("~/Downloads/MSDS-Orientation-Computer-Survey.xlsx")
 View(MSDS_Orientation_Computer_Survey)
 
-hist(MSDS_Orientation_Computer_Survey$`CPU Number of Cores (int)`, xlab = "CPU Number of Cores", col = "red",
-     main = "Number of Cores of MSDS students' CPUs")
+ggplot(MSDS_Orientation_Computer_Survey, aes(x = MSDS_Orientation_Computer_Survey$`CPU Number of Cores (int)`))+
+  geom_histogram(fill = "red", col = "black") + xlab("CPU Number of Cores") + ggtitle("MSDS students' CPUs Number of Cores")+
+  ylab("frequency")
