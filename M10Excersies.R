b4eb3a6ae28f06909dfacb3a5034bbfd77417598
kurt-jr <nps3cs@virginia.edu>
2024-07-03 10:52:27 -0400

    kk

--- M10Excersies.R
+++ M10Excersies.R
@@ -1,9 +1,22 @@
 #10.1
-
+#first 10 fibonacci 
 fib <- numeric(10)  # Create a numeric vector of length n
 fib[1] <- 1
 fib[2] <- 1
 for (i in 3:10) {
   fib[i] <- fib[i-1] + fib[i-2]
 }
-return(fib)
+print(fib)
+
+#10.2
+fibonacci <- function(n) {
+  fib <- numeric(n)  # Create a numeric vector of length n
+  fib[1] <- 1
+  fib[2] <- 1
+  for (i in 3:n) {
+    fib[i] <- fib[i-1] + fib[i-2]
+  }
+  return(fib)
+}
+
+fibonacci(10)
